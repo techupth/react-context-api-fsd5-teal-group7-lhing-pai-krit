8a13084cfeff8e260a43e7ed8bb4c5cc4938a9f8
Krit <titatit.akkh@gmail.com>
2024-01-03 13:57:10 +0700

    finished Ex: context-api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import { UserContext } from "./context/UserContext.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
 
 function App() {
+  // const [user, setUser] = useState([]);
+
   const userData = {
     username: "John",
     avatar: "https://placedog.net/100/100",
@@ -12,14 +15,23 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <UserContext.Provider
+      value={{
+        userState: userData,
+      }}
+    >
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage userState={userData} />} />
+            <Route
+              path="/product/view/:id"
+              element={<ViewProductPage userState={userData} />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
 function AppHeader() {
+  const userData = useContext(UserContext);
+
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {userData.userState.username}</h2>
     </div>
   );
 }
